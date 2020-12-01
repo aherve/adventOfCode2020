6010bf7c346f21bbdd9943cf1d6456f76fbc4c2a
Aurélien Hervé <aurelien@aircall.io>
2020-12-01 16:52:36 +0100

    ruby part2

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,1 +1,2 @@
 p File.read('./input.txt').split("\n").map{|x| x.to_i}.permutation(2).to_a.select{|x,y| x + y == 2020}.first.reduce(:*)
+p File.read('./input.txt').split("\n").map{|x| x.to_i}.permutation(3).to_a.select{|x,y,z| x + y + z == 2020}.first.reduce(:*)
