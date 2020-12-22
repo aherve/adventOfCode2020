a8386be5d03407ee6240a796a0af71ba545c1de2
Aurélien Hervé <aurelien@aircall.io>
2020-12-22 14:35:34 +0100

    day12

--- day12/main.go
+++ day12/main.go
@@ -7,35 +7,79 @@ import (
 	"strings"
 )
 
-type shipStr struct {
-	Direction int
-	EastPos   int
-	NorthPos  int
-}
+type (
+	shipStr struct {
+		Direction int
+		EastPos   int
+		NorthPos  int
+		Waypoint  *WaypointStr
+	}
+
+	WaypointStr struct {
+		EastPos  int
+		NorthPos int
+	}
+)
 
 func newShip() shipStr {
 	return shipStr{
 		Direction: 0,
 		EastPos:   0,
 		NorthPos:  0,
+		Waypoint:  &WaypointStr{10, 1},
 	}
 }
 
 func main() {
-	ship := newShip()
+	ship1, ship2 := newShip(), newShip()
 	for _, instr := range slurpFile("./input.txt") {
-		ship.do(instr)
+		ship1.part1Navigate(instr)
+		ship2.part2Navigate(instr)
 	}
-	log.Printf("part1 => %v", ship.ManhattanDist())
+	log.Printf("part1 => %v", ship1.ManhattanDist())
+	log.Printf("part2 => %v", ship2.ManhattanDist())
 }
 
-func (ship *shipStr) do(instr string) {
-	command := instr[0]
-	argStr := instr[1:]
+func (ship *shipStr) part2Navigate(instr string) {
+	command, arg := readInstruction(instr)
+
+	switch command {
+	case 'N':
+		ship.Waypoint.NorthPos += arg
+	case 'S':
+		ship.Waypoint.NorthPos -= arg
+	case 'E':
+		ship.Waypoint.EastPos += arg
+	case 'W':
+		ship.Waypoint.EastPos -= arg
+	case 'L':
+		ship.Waypoint.rotate(arg)
+	case 'R':
+		ship.Waypoint.rotate(-1 * arg)
+	case 'F':
+		ship.NorthPos += arg * ship.Waypoint.NorthPos
+		ship.EastPos += arg * ship.Waypoint.EastPos
+	}
+}
+
+func (wp *WaypointStr) rotate(degres int) {
+	for i := 0; i < ((degres+360)%360)/90; i++ { // convert to a positive number of left turns
+		wp.EastPos, wp.NorthPos = -wp.NorthPos, wp.EastPos // apply left turn
+	}
+}
+
+func readInstruction(input string) (byte, int) {
+	command := input[0]
+	argStr := input[1:]
 	arg, err := strconv.Atoi(argStr)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return command, arg
+}
+
+func (ship *shipStr) part1Navigate(instr string) {
+	command, arg := readInstruction(instr)
 
 	switch command {
 	case 'N':
